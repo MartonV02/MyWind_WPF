0dd468af4b8ebb47fa3325c0402de9f99d401dab
MartonV02 <90245864+MartonV02@users.noreply.github.com>
2022-05-21 23:29:59 +0200

    Add files via upload

---
+++ 2022-02-28 MyWind - WPF/2022-02-28 MyWind - WPF/Employee.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2022_02_28_MyWind___WPF
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Notes { get; set; }
+
+        public string FullName => $"{FirstName} {LastName.ToUpper()}".Trim();
+
+        public Employee()
+        { }
+
+        public Employee(MySqlDataReader reader)
+        {
+            this.Id = (int)reader["id"];
+            this.FirstName = reader["first_name"].ToString();
+            this.LastName = reader["last_name"].ToString();
+            this.Email = reader["email_address"].ToString();
+            this.Notes = reader["notes"].ToString();
+        }
+
+        public Employee(Employee employee)
+        {
+            this.Id = employee.Id;
+            this.FirstName = employee.FirstName;
+            this.LastName = employee.LastName;
+            this.Email = employee.Email;
+            this.Notes = employee.Notes;
+
+        }
+    }
+}
---
+++ 2022-02-28 MyWind - WPF/2022-02-28 MyWind - WPF/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2022_02_28_MyWind___WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //string connectionString = "User Id=root;Host=localhost;Database=northwind";
+        string connectionString = "Uid=root;Host=localhost;Database=northwind";
+        ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (var con = new MySqlConnection(connectionString))
+            {
+                con.Open();
+                var sql = "select id, " +
+                                 "first_name, " +
+                                 "last_name, " +
+                                 "email_address, " +
+                                 "notes " +
+                          " from employees";
+                using(var cmd = new MySqlCommand(sql, con))
+                    using (var reader = cmd.ExecuteReader())
+                        while (reader.Read())
+                            employees.Add(new Employee(reader));
+                
+                dgList.ItemsSource = employees;
+                isEditMode(false);
+            }
+        }
+
+        private void isEditMode(bool isEditMode)
+        {
+            txtFirstName.IsReadOnly = !isEditMode;
+            txtLastName.IsReadOnly = !isEditMode;
+            txtEmail.IsReadOnly = !isEditMode;
+            txtNotes.IsReadOnly = !isEditMode;
+
+            dgList.IsEnabled = !isEditMode;
+            pnlModify.Visibility = isEditMode ? Visibility.Hidden : Visibility.Visible; 
+            pnlSave.Visibility = !isEditMode ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        private void dgList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            grpData.DataContext = dgList.SelectedItem;
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+            grpData.DataContext = new Employee();
+            isEditMode(true);            
+        }
+
+        private void btnModify_Click(object sender, RoutedEventArgs e)
+        {
+            if (grpData.DataContext != null)
+            {
+                //TODO: adott rekord adatbázisból lekérdezése -> változhatott az ablak megnyitása óta
+                grpData.DataContext = new Employee( (Employee)grpData.DataContext );
+                isEditMode(true);
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: kötelező mező ellenőrzés
+            var employee = (Employee)grpData.DataContext;
+            if (employee.Id == 0)
+            {
+                using (var con = new MySqlConnection(connectionString))
+                {
+                    con.Open();
+                    var sql = "insert into employees(first_name, last_name, email_address, notes) " +
+                              "values (@first_name, @last_name, @email_address, @notes); " +
+                              "select last_insert_id() as id";
+                    using (var cmd = new MySqlCommand(sql, con))
+                    {
+                        cmd.Parameters.AddWithValue("@first_name", employee.FirstName);
+                        cmd.Parameters.AddWithValue("@last_name", employee.LastName);
+                        cmd.Parameters.AddWithValue("@email_address", employee.Email);
+                        cmd.Parameters.AddWithValue("@notes", employee.Notes);
+
+                        employee.Id = Convert.ToInt32(cmd.ExecuteScalar());
+                        employees.Add(employee);
+                        dgList.SelectedItem = employee;
+                        isEditMode(false);
+                    }
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            grpData.DataContext = null;
+            isEditMode(false);
+        }
+
+        
+    }
+}
